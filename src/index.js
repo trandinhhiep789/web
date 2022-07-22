af1e7e8048280e9180aaea9527837ce9ca619df5
eliot <trandinhhiep789@gmail.com>
2022-07-22 12:13:15 +0700

    setup: images resource

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import "./styles/main.scss"
+import abc from "./images/Diamond.png"
 
 // Tạo component App
 function App() {
     return (
         <div className="container">
             <h1>TMS</h1>
+            <img src={abc}/>
         </div>
     )
 }
--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,10 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 
 const plugins = [
+  new MiniCssExtractPlugin(),
   new HtmlWebpackPlugin({
     template: "./index.html"
-  }),
-  new MiniCssExtractPlugin()
+  })
 ];
 
 module.exports = {
@@ -16,6 +16,7 @@ module.exports = {
     path: path.join(__dirname, "/dist"),
     filename: "[name].[contenthash].js", // setup caching cho web chạy nhanh hơn 
     clean: true, // xét true để mỗi lần `npm run build` thì nó sẽ lấy file bản mới thay cho bản cũ, mặc định là nó không thay thế vì chỗ này đã setup caching cho web chạy nhanh hơn  
+    publicPath: '/'
   },
   devtool: 'source-map',
   devServer: {
@@ -35,9 +36,40 @@ module.exports = {
       }, 
       {
         test: /\.s?css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader", "sass-loader"]
-        // according to the docs, sass-loader should be at the bottom, which
-        // loads it first to avoid prefixes in your sourcemaps and other issues.
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            // This is required for asset imports in CSS, such as url() in background-image
+            options: { publicPath: "" },
+          },
+          "css-loader",
+          "postcss-loader",
+          // according to the docs, sass-loader should be at the bottom, which
+          // loads it first to avoid prefixes in your sourcemaps and other issues.
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        /**
+         * The `type` setting replaces the need for "url-loader"
+         * and "file-loader" in Webpack 5.
+         *
+         * setting `type` to "asset" will automatically pick between
+         * outputing images to a file, or inlining them in the bundle as base64
+         * with a default max inline size of 8kb
+         */
+        type: "asset",
+
+        /**
+         * If you want to inline larger images, you can set
+         * a custom `maxSize` for inline like so:
+         */
+        // parser: {
+        //   dataUrlCondition: {
+        //     maxSize: 30 * 1024,
+        //   },
+        // },
       }
     ]
   },
